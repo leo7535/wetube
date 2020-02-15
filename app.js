c66992d07cc717a854f147234be4a98a8d4b716b
leo7535 <35109302+leo7535@users.noreply.github.com>
2020-02-16 02:30:03 +0900

    macOS

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router1"; //default로 export하지 않았을 때에는 이렇게
+import userRouter from "./routers/userRouter"; //default로 export하지 않았을 때에는 이렇게
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+import routes from "./routes";
 
 const app = express();
-
-const serverHome = (req, res) => res.send("Hello World !");
-const serverProfile = (req, res) => res.send("My Profile ");
 
 app.use(cookieParser());
 app.use(bodyParser.json());
@@ -16,8 +16,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));
 app.use(helmet());
 
-app.get("/", serverHome);
-app.get("/profile", serverProfile);
-app.use("/user", userRouter); // user경로에 접속하면 이 router 전체를 사용하겠다는 의미
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app; //누군가 app파일을 import 할 때 app object를 주겠다는 의미
